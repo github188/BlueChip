b520b786c05f84903388acbe09bb35d3d60895c4
LUO Yun <lauren.luo@foxmail.com>
2015-07-12 19:50:14 +0800

    test gps
    test gps

--- src/con_gps.cc
+++ src/con_gps.cc
@@ -149,7 +149,7 @@ int CConGPS::ShowLocation(){
 						std::cout<<latitude<<" "<<longitude<<endl;
 				}
 				for(int i=0;i<g_latitude.size()-1;i++){
-                        if(abs(latitude-g_latitude[i])<0.000000001&&abs(longitude-g_longitude[i])<0.000000001){
+                        if((fabs(latitude-g_latitude[i])<0.000000001)&&(fabs(longitude-g_longitude[i])<0.000000001)){
 								//@return 2:xinlihua
 								//@return 3:xinhaoi
 								//@return 4:leave xinhaoi
